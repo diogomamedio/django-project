90b393e2d6800d6aa496423eebd7840b577aaade
Diogo Marcelo Mamédio <diogomamedio@yahoo.com.br>
2025-05-28 10:22:47 -0300

    Added styles and statics files

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
-
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html',context={
-        'name': 'João'
+    return render(request, 'recipes/pages/home.html',context={
+        'name': 'Diogo Marcelo'
     })
-
-def sobre(request):
-    return HttpResponse("Sobre Django!")
-def contato(request):
-    return HttpResponse("Contato Django!")
