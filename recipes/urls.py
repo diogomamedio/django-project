5d5b0768507858c402c9893902b052e67613d99e
Diogo Marcelo Mamédio <diogomamedio@yahoo.com.br>
2025-06-02 07:26:52 -0300

    Update Software

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from recipes.views import home
+from recipes.views import home, recipe
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
+    path('recipes/<int:id>/', recipe, name='recipe'),
 ]
